ebd6fe00bfd0a09355ee49d8c85c6e1bfd563f81
Ayan Pal <APal@ESTUATE.COM>
2025-06-03 16:54:41 +0530

    Package changes commit

---
+++ services/cards/src/main/java/com/estuate/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.estuate.cards.repository;
+
+import com.estuate.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
